c7263d9e3a1c71ffd76962eef92ea902c34fe404
Alexis Burgos <alexisburgosgtm@outlook.com>
2020-09-08 21:03:43 -0600

    cambio de color de la tabla

--- build/controllers/classDb.js
+++ build/controllers/classDb.js
@@ -73,7 +73,6 @@ let classDb = {
         let contenedor = document.getElementById(idContainer);
         
         let tbl = `<div class="table-responsive col-12">
-        <h5 class="text-danger">Clientes Sin Enviar</h5>
         <table class="table table-responsive table-hover table-striped">
             <thead class="bg-danger text-white">
                 <tr>
