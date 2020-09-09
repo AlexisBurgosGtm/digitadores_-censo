6902fee6bcc063f315c683bf4485df7e443a046d
Alexis Burgos <alexisburgosgtm@outlook.com>
2020-09-08 19:44:08 -0600

    version en login

--- build/views/login/index.js
+++ build/views/login/index.js
@@ -4,7 +4,7 @@ function getView(){
             return `
         <div class="row">
             <div class="col-md-4 col-sm-12 col-lg-4 col-lx-4">
-                
+                <h3>Ver 1</h3>
             </div>
 
             <div class="col-md-4 col-sm-12 col-lg-4 col-lx-4">
