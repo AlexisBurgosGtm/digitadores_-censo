03891c098407872733832271258aa36320abf9ff
Alexis Burgos (gtm) <34293632+AlexisBurgosGtm@users.noreply.github.com>
2021-04-08 08:37:06 -0600

    inicial

--- router/connection.js
+++ router/connection.js
@@ -1,12 +1,10 @@
-﻿
-const config = {
-	user: 'DB_A6478C_mercadosv2_admin',
+﻿const config = {
+	user: 'DB_A6478C_digitadoresremotos_admin',
 	password: 'razors1805',
-	server: 'sql5060.site4now.net',
-	database: 'DB_A6478C_mercadosv2',
+	server: 'sql5068.site4now.net',
+	database: 'DB_A6478C_digitadoresremotos',
 	pool: {	max: 100,	min: 0,	idleTimeoutMillis: 30000}
 };
-
 
 
 
